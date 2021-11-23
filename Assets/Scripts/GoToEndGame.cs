421a2cf4f57a683658071d274937ab8b143a2cf9
Vladislav529 <iemm@rambler.ru>
2021-11-23 12:59:21 +0300

    Fix End

--- Assets/Scripts/GoToEndGame.cs
+++ Assets/Scripts/GoToEndGame.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GoToEndGame : BaseWindow
+public class GoToEndGame : MonoBehaviour
 {
     public GameObject character;
+    public GameObject girl;
+    public WindowManager windowManager;
     private float endDistance = 6f;
     private bool isEnd = false;
 
     public void Update()
     {
-        if (Vector3.Distance(character.transform.position, this.transform.position) < endDistance && !isEnd)
+        if (Vector3.Distance(character.transform.position, girl.transform.position) < endDistance && !isEnd)
         {
-            _windowManager.ShowWindow("EndGameWindow");
+            windowManager.ShowWindow("EndGameWindow");
             isEnd = true;
         }
     }
